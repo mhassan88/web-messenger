6a3e2286ac49d75cfb4576d2ceaffc3cc6080419
mhassan88 <69553703+mhassan88@users.noreply.github.com>
2021-01-10 13:10:47 -0500

    Initial Commit
    created directories for pages. and created singup.jsx and css file.
    removed some files which were present by default in the template.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,10 @@
 import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
-
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
-
 import "./App.css";
+import SignupPage from './pages/signup/signup';
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <SignupPage/>
   );
 }
 
