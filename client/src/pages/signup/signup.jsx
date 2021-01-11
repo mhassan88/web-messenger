67b36b0f3658d2002845cd0387cda86f30adf897
mhassan88 <69553703+mhassan88@users.noreply.github.com>
2021-01-11 07:25:50 -0500

    Applied Form validation for signup page

--- client/src/pages/signup/signup.jsx
+++ client/src/pages/signup/signup.jsx
@@ -1,12 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCommentDots } from "@fortawesome/free-regular-svg-icons";
 import bgImg from "../../assets/images/bg-img.png";
 import "./signup.css";
 import { Container, Grid, Button, TextField } from "@material-ui/core";
+import Joi from "joi";
+import FormComponent from "../../common/form";
 
-class signupPage extends Component {
-  state = {};
+class signupPage extends FormComponent {
+  state = {
+    data: { username: "", email: "", password: "", confirmPassword: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().alphanum().label("Username"),
+    email: Joi.string()
+      .required()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .label("Email"),
+    password: Joi.string().required().min(6).alphanum().label("Password"),
+    confirmPassword: Joi.ref("password"),
+  };
+  doSubmit = () => {
+    //call to server
+    console.log("submitted ");
+  };
+
   render() {
     return (
       <div>
@@ -28,7 +48,7 @@ class signupPage extends Component {
               </div>
             </Grid>
             <Grid item xs={12} sm={12} md={6} lg={7} xl={7}>
-              <Grid container spacing={10}>
+              <Grid container spacing={7}>
                 <Grid item xs={12}>
                   <div className="margin-top align-right">
                     <span className="text-style">Already have an account?</span>
@@ -42,49 +62,61 @@ class signupPage extends Component {
                   <form autoComplete="off">
                     <TextField
                       type=""
-                      error={false}
+                      error={!(this.state.errors.username == null)}
+                      name="username"
                       id="username"
                       label="Username"
-                      defaultValue=""
-                      helperText=""
+                      value={this.state.data.username}
+                      helperText={this.state.errors.username}
+                      onChange={this.handleChange}
                     />
                     <br />
                     <br />
                     <br />
                     <TextField
                       type=""
-                      error={false}
+                      error={!(this.state.errors.email == null)}
+                      name="email"
                       id="email"
                       label="Email address"
-                      defaultValue=""
-                      helperText=""
+                      value={this.state.data.email}
+                      helperText={this.state.errors.email}
+                      onChange={this.handleChange}
                     />
                     <br />
                     <br />
                     <br />
                     <TextField
                       type="password"
-                      error={false}
+                      error={!(this.state.errors.password == null)}
+                      name="password"
                       id="password"
                       label="Password"
-                      defaultValue=""
-                      helperText=""
+                      value={this.state.data.password}
+                      helperText={this.state.errors.password}
+                      onChange={this.handleChange}
                     />
                     <br />
                     <br />
                     <br />
                     <TextField
                       type="password"
-                      error={false}
+                      error={!(this.state.errors.confirmPassword == null)}
+                      name="confirmPassword"
                       id="confirmPassword"
                       label="Confirm Password"
-                      defaultValue=""
-                      helperText=""
+                      value={this.state.data.confirmPassword}
+                      helperText={this.state.errors.confirmPassword}
+                      onChange={this.handleChange}
                     />
                     <br />
                     <br />
                     <br />
-                    <Button variant="contained" color="primary">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleSubmit}
+                    >
                       Create
                     </Button>
                   </form>
